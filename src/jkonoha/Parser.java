90b28640b6abcbe644fbebbcf507e3f169c29fb7
JosephLee <leejonggun@gmail.com>
2012-05-30 13:10:22 +0900

    Edit Parser.java

--- src/jkonoha/Parser.java
+++ src/jkonoha/Parser.java
@@ -5,8 +5,20 @@ import java.util.*;
 public class Parser {
 
 	// important
-	public Block newBlock() {
+	public Block newBlock(CTX ctx, KonohaSpace ks, Stmt parent, List<Token> tls, int s, int e, int delim) {
 		//TODO
+		Block bk = new Block();
+		int i = s, indent = 0, atop = tls.size();
+		while (i < e) {
+			Token tkERR;
+			assert (atop == tls.size());
+			i = selectStmtLine (ctx, ks, indent, tls, i, e, delim, tls, tkERR);//TODO How to tkERR?
+			int asize = tls.size();
+			if (asize > atop) {
+				Block_addStmtLine (ctx, bk, tls, atop, asize, tkERR);
+				tls.remove(atop);
+			}
+		}
 		return null;
 	}
 	
@@ -15,8 +27,18 @@ public class Parser {
 				
 	}
 	
-	public void selectStmtLine() {
+	public int selectStmtLine(CTX ctx, KonohaSpace ks, int indent, List<Token> tls, int s, int e, int delim, List<Token> tlsdst, Token tkERRRef) {
 		//TODO
+		return 1;
+	}
+	
+	public void Block_addStmtLine (CTX ctx, Block bk, List<Token> tls, int s, int e, Token tkERR) {
+		//TODO
+		Stmt stmt = new Stmt(s/*tls.toks[s].uline*/);
+		stmt.add(bk);
+		if (tkERR != null) {
+			
+		}
 		
 	}
 	
