ddade449a45e3460b374454a07f49ebedf641a12
kanemura1206 <kanemura1206@gmail.com>
2012-06-01 14:39:38 +0900

    merge wakama2

--- src/jkonoha/KonohaSpace.java
+++ src/jkonoha/KonohaSpace.java
@@ -3,15 +3,15 @@ package jkonoha;
 import java.util.*;
 
 public class KonohaSpace extends KObject {
-	
+
 	public KonohaSpace parentNULL;
 	public FTokenizer[] fmat;
 	public Map<String, Syntax> syntaxMapNN = new HashMap<String, Syntax>();
-	
+
 	public KonohaSpace() {
 		//TODO
 	}
-	
+
 	public void tokenize(CTX ctx, String source, long uline, List<Token> toks) {
 		int i, pos = toks.size();
 		TEnv tenv = new TEnv(source, uline, toks, 4, this);
@@ -22,134 +22,21 @@ public class KonohaSpace extends KObject {
 			}
 		}
 	}
-	
-	public void eval(CTX ctx, String script, long uline) {
-		// TODO
-		List<Token> tls = new ArrayList<Token>();
-		tokenize(script, uline, tls);
-		Block bk = null;//newBlock();
-		//cctx.evalBlock(bk);
-	}
-	
-	public void tokenize(String source, long uline, List<Token> a) {
-		//TODO
-	}
-	
-	public boolean importPackage(String name, long pline) {
-		//TODO
-		return false;
-	}
-	
-	public boolean loadScript(String path) {
-		//TODO
-		return false;
-	}
 
-
-	public Syntax getSyntaxRule(CTX ctx, Stmt stmt, List<Object> tls, int s, int e) {
-		//TODO
-		Token tk = (Token)tls.get(s);
-		if (tk.kw == KW.Type) {
-			
-		}
-		return null;
-	}
-}
-
-class TEnv {
-	
-	public String source;
-	public long uline;
-	public ArrayList<Token> list;
-	public int bol;   // begin of line
-	public int indent_tab;
-	public FTokenizer[] fmat;
-	
-	/**
-	 * constructer
-	 * sourcecode
-	 * @param source
-	 * which line
-	 * @param uline
-	 * tokens
-	 * @param a
-	 * indent
-	 * @param indent_tab
-	 * parser
-	 * @param fmat
-	 */
-	
-	TEnv(String source, long uline, ArrayList<Token> a, int indent_tab, KonohaSpace ks) {
-		this.source = source;
-		this.uline = uline;
-		this.list = a;
-		this.bol = 0;
-		this.indent_tab = indent_tab;
-		this.fmat = Tokenizer.MiniKonohaTokenMatrix(); // TODO
-	}
-	
-//	/**
-//	 * This method is used to know the position of line.
-//	 * @param pos
-//	 * @return int
-//	 */
-//	
-//	public final int lpos(int pos) {
-//		return (this.bol == 0) ? -1 : (int)(pos - this.bol);
-//	}
-}
-
-final class ParseINDENT implements FTokenizer {
-	
-	@Override public final int parse(CTX ctx,  Token tk, TEnv tenv, int pos, Method thunk) {
-		int ch, c = 0;
-//		while((ch = tenv.source.charAt(pos++)) != 0) {
-		while(true) {
-			pos++;
-			if(pos >= tenv.source.length()) break;
-			if((ch = tenv.source.charAt(pos)) == 0) break;
-			if(ch == '\t') { c += tenv.indent_tab; }
-			else if(ch == ' ') { c += 1; }
-			break;
-		}
-		if(tk != null/* TODO IS_NOTNULL(tk) */) {
-			tk.tt = Token.TK_INDENT;
-//			tk.lpos = 0;		
-		}
-		return pos - 1;
-	}
-}
-
-final class ParseNL implements FTokenizer {
-	
-	@Override public final int parse(CTX ctx,  Token tk, TEnv tenv, int pos, Method thunk) {
-		tenv.uline += 1;
-		tenv.bol = pos + 1;
-		return Tokenizer.parseINDENT.parse(ctx, tk, tenv, pos + 1, thunk);
-	}
-}
-
-final class ParseNUM implements FTokenizer {
-	
-	@Override public final int parse(CTX ctx,  Token tk, TEnv tenv, int tok_start, Method thunk) {
-		int ch, pos = tok_start, dot = 0;
-		String ts = tenv.source;
-//		while((ch = ts.charAt(pos++)) != 0) {
-		while(true) {
-			pos++;
-			if(pos >= ts.length()) break;
-			if((ch = ts.charAt(pos)) == 0) break;
-			if(ch == '_') continue; // nothing
-			if(ch == '.') {
-				if(!Character.isDigit(ts.charAt(pos))) {
-					break;
-				}
-				dot++;
-				continue;
-			}
-			if((ch == 'e' || ch == 'E') && (ts.charAt(pos) == '+' || ts.charAt(pos) == '-')) {
-				pos++;
-				continue;
+	private void tokenize(CTX ctx, TEnv tenv) {
+		int ch, pos = 0;
+		FTokenizer fmat[] = tenv.fmat;
+		RawToken tk = new RawToken(tenv.uline);
+		assert(tk.tt == 0);
+		tk.uline = tenv.uline;
+//		tk.lpos = tenv.lpos(0);
+		pos = Tokenizer.parseINDENT.parse(ctx, tk, tenv, pos, null);
+		while(pos < tenv.source.length() && (ch = Tokenizer.kchar(tenv.source, pos)) != 0) {
+			if(tk.tt != 0) {
+				tenv.list.add(tk);
+				tk = new RawToken(tenv.uline);
+				tk.uline = tenv.uline;
+				//tk.lpos = tenv.lpos(pos);
 			}
 			int pos2 = fmat[ch].parse(ctx, tk, tenv, pos, null);
 			assert pos2 > pos;
@@ -159,80 +46,96 @@ final class ParseNUM implements FTokenizer {
 			tenv.list.add(tk);
 		}
 	}
-	
+
 	public FTokenizer[] tokenizerMatrix(CTX ctx) {
 		//TODO
 		return null;
 	}
-	
+
 	public void setTokenizer(int ch, FTokenizer f, KMethod mtd) {
 		//TODO
 	}
-	
+
 	public Syntax getSyntaxRule(CTX ctx, List<Token> tls, int s, int e) {
 		//TODO
 		return null;
 	}
-	
+
 	public void syntax(CTX ctx, String kw, int isnew) {
 		//TODO
 	}
-	
+
 	public void defineSyntax(CTX ctx, Syntax[] syndef) {
 		//TODO
 	}
-	
+
 	public void setSyntaxMethod(CTX ctx, KMethod f, KMethod[] synp, KMethod p, KMethod[] mp) {
 		//TODO
 	}
-	
+
 	public void addMethod(CTX ctx, KMethod mtd) {
 		//TODO
 	}
-	
+
 	public KMethod getMethodNULL(CTX ctx, int cid, String mn) {
 		//TODO
 		return null;
 	}
-	
+
 	public KMethod getStaticMethodNULL(CTX ctx, String mn) {
 		//TODO
 		return null;
 	}
-	
+
 	public boolean defineMethod(CTX ctx, KMethod mtd, long pline) {
 		//TODO
 		return false;
 	}
-	
+
 	public void loadMethodData(CTX ctx, Object data) {
 		//TODO
 	}
-	
+
+	public KClass getCT(CTX ctx, KClass thisct, String name, int def) {
+		//TODO
+		return null;
+	}
+
 	public void eval(CTX ctx, String script, long uline) {
 		List<Token> tls = new ArrayList<Token>();
 		tokenize(ctx, script, uline, tls);
-		
+
+		// debug: dump tokens
+		for(int i = 0; i < tls.size(); i++) {
+			RawToken rtk = (RawToken)tls.get(i);
+			System.out.print("{ token type:" + rtk.tt + ", ");
+			if(rtk.text != null) {
+				System.out.print("text: " + rtk.text + ", ");
+			}
+			else {
+				System.out.print("text: null, ");
+			}
+			System.out.println("uline: " + rtk.uline + " }");
+		}
+
 		Parser p = new Parser();
 		int pos = tls.size();
 		Block bk = p.newBlock(ctx, this, null, tls, pos, tls.size(), ';');
 		evalBlock(ctx, bk);
 	}
-	
+
 	private void evalBlock(CTX ctx, Block bk) {
-		//Block bk1 = ctx.sugar.singleBlock;
-		//KMethod mtd = null;//TODO
+		//TODO
 	}
-	
+
 	public boolean importPackage(CTX ctx, String name, long pline) {
 		//TODO
 		return false;
 	}
-	
+
 	public boolean loadScript(CTX ctx, String path) {
 		//TODO
 		return false;
 	}
-	
-}
 
+}
