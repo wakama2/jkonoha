5a618ea1a894699b20d9c74c5958ec3a6ed18bef
kanemura1206 <kanemura1206@gmail.com>
2012-06-20 17:01:31 +0900

    make input method

--- src/jkonoha/Konoha.java
+++ src/jkonoha/Konoha.java
@@ -1,5 +1,6 @@
 package jkonoha;
 
+import java.io.*;
 import java.util.*;
 
 public class Konoha {
@@ -43,8 +44,67 @@ public class Konoha {
 		}
 	}
 	
-	public void load(String script) {
-		// TODO
+	private void loadScript(CTX ctx, String path) {
+		BufferedReader br = null;
+		try {
+			File inputFile = new File(path);
+			br = new BufferedReader(new FileReader(inputFile));
+			String msg;
+			String script = "";
+			boolean check = false;
+			while ((msg = br.readLine()) != null) {
+				script = script + msg;
+				check = checkStmt(script);
+				if (check){
+					this.eval(ctx, script);
+					script = "";
+				}
+			}
+			if (!check) System.out.println("(Cancelled)...");
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null){
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	private boolean checkStmt(String path) {
+		int i = 0, ch, nest = 0, quote = 0;
+		while (true){
+			int flag = 0;
+			for (; i < path.length(); i++){
+				ch = path.charAt(i);
+				if(ch == '{' || ch == '[' || ch == '(') nest++;
+				if(ch == '}' || ch == ']' || ch == ')') nest--;
+				if(ch == '\'' || ch == '"' || ch == '`') {
+					if(path.charAt(i+1) == ch && path.charAt(i+2) == ch) {
+						quote = ch; i+=2;
+						flag = 1;
+						break;
+					}
+				}
+			}
+			if (flag == 0) return nest == 0;
+			assert(i > 0);
+			flag = 0;
+			for(; i < path.length(); i++) {
+				ch = path.charAt(i);
+				if(path.charAt(i-1) != '\\' && ch == quote) {
+					if(path.charAt(i+1) == ch && path.charAt(i+2) == ch) {
+						i+=2;
+						flag = 1;
+						break;
+					}
+				}
+			}
+			if (flag == 0) return false;
+		}
 	}
 	
 //	public Object eval(String script, long uline) {
@@ -55,12 +115,30 @@ public class Konoha {
 		CTX ctx = new CTX();
 		Konoha k = new Konoha(ctx);
 		Scanner s = new Scanner(System.in);
-		while(true) {
-			System.out.print(">>>");
-			String l = s.nextLine();
-			if(l == null) break;
-			k.eval(ctx, l);
+		if (args.length == 1) {
+			String path = args[0];
+			k.loadScript(ctx, path);
 		}
+		else if (args.length == 0){
+			while(true) {
+				System.out.print(">>>");
+				String script = "";
+				String l;
+				int flag;
+				while ((l = s.nextLine()) != null) {
+					flag = 0;
+					script = script + l;
+					if (k.checkStmt(script)){
+						k.eval(ctx, script);
+						script = "";
+						flag = 1;
+					}
+					if (flag == 0) System.out.print("   ");
+					else System.out.print(">>>");
+				}
+			}
+		}
+		else {}// TODO option
 	}
 	
 }
