e6b86fe796496bdae0a8a4090f2023982447e230
kanemura1206 <kanemura1206@gmail.com>
2012-06-22 13:46:23 +0900

    not change

--- src/test/IntOperatorCmpMoreThan.java
+++ src/test/IntOperatorCmpMoreThan.java
@@ -14,12 +14,12 @@ public class IntOperatorCmpMoreThan {
 		Konoha k = new Konoha(ctx);
 		int i1 = 1;
 		int i2 = 2;
-//		k.eval(ctx,  "int i1 = 1;");
-//		k.eval(ctx,  "int i2 = 2;");
 		assertEquals(1 > 2, ((KBoolean) k.eval(ctx, "1 > 2")).unbox());
-//		assertEquals(1 > i1, ((KBoolean) k.eval(ctx, "1 > i1")).unbox());
-//		assertEquals(i1 > 1, ((KBoolean) k.eval(ctx, "i1 > 1")).unbox());
-//		assertEquals(i1 > i2, ((KBoolean) k.eval(ctx, "i1 > i2")).unbox());
+		k.eval(ctx, "boolean f(){int i1 = 1; return 1 > i1;}");
+		assertEquals(1 > i1, ((KBoolean) k.eval(ctx, "f()")).unbox());
+		k.eval(ctx, "boolean f(){int i1 = 1; retrun i1 > 1;}");
+		assertEquals(i1 > 1, ((KBoolean) k.eval(ctx, "f()")).unbox());
+		k.eval(ctx, "boolean f(){int i1 = 1; int i2 = 2; retrun i1 > i2;}");
+		assertEquals(i1 > i2, ((KBoolean) k.eval(ctx, "f()")).unbox());
 	}
-
 }
